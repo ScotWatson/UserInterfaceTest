99ab673643fc4b2616105552bfb3f302088483a8
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-01-05 09:18:03 -0500

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -3,12 +3,20 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-const loading = new Promise((resolve, reject) => {
-  window.addEventListener("load", resolve);
+const pageLoadStart = performance.now();
+
+const loadingPage = new Promise((resolve, reject) => {
+  window.addEventListener("load", (e) => {
+    resolve({
+      loadTime: performance.now() - pageLoadStart,
+    });
+  });
 });
 
-loading.then(start);
+const loadingUI = new Promise();
 
-function start() {
-  
+Promise.all([ loadingPage, loadingUI ]).then(start);
+
+function start([ Page, UI ]) {
+  UI.initialize();
 }
