3fd92989a016a1693e122f677373009931131bed
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-01-18 21:51:24 -0500

    Update ui.mjs

--- docs/ui.mjs
+++ docs/ui.mjs
@@ -3,6 +3,8 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+import "AsyncEvents" from "./async-events.mjs";
+
 const urlSelf = new URL(self.location);
 const urlIconUnselected = new URL("./icons/unselected.svg", urlSelf);
 const urlIconRadioSelected = new URL("./icons/radio-selected.svg", urlSelf);
@@ -1180,47 +1182,4 @@ function createVerticalScrollable() {
     div,
     obj,
   };
-}
-// Implements the async iterable interface
-class EventGenerator {
-  #next;
-  constructor(init) {
-    let _resolve = null;
-    let _reject = null;
-    const next = () => {
-      this.#next = new Promise((resolve, reject) => {
-        _resolve = resolve;
-        _reject = reject;
-      });
-    }
-    function generate(value) {
-      _resolve({
-        value,
-        done: false,
-      });
-      next();
-    }
-    function final(value) {
-      _resolve({
-        value,
-        done: false,
-      });
-      next();
-    }
-    function reject(reason) {
-      _reject(reason);
-      next();
-    }
-    init(generate, final, reject);
-    next();
-  }
-  [Symbol.asyncIterator]() {
-    return {
-      next: () => {
-        return new Promise((resolve, reject) => {
-          this.#next.then(resolve, reject);
-        });
-      },
-    };
-  }
 }
