d665ee57bb60d1de2217c0ecaceaf16514ede882
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-01-19 08:13:22 -0500

    Update async-events.mjs

--- docs/async-events.mjs
+++ docs/async-events.mjs
@@ -4,6 +4,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 // Implements the async iterable interface
+// Produces data independent of consumption, therefore a push source
 class EventIterable {
   #next;
   constructor(subscribe) {
@@ -16,30 +17,33 @@ class EventIterable {
         _reject = reject;
       });
     }
-    function next(value) {
-      _resolve({
-        value,
-        done: false,
-      });
-      waitForInput();
-    }
-    function complete(value) {
-      _resolve({
-        value,
-        done: false,
-      });
-      waitForInput();
-    }
-    function error(reason) {
-      _reject(reason);
-      waitForInput();
-    }
-    subscribe({ next, complete, error });
+    const observer = {
+      next(value) {
+        _resolve({
+          value,
+          done: false,
+        });
+        waitForInput();
+      },
+      complete(value) {
+        _resolve({
+          value,
+          done: false,
+        });
+        waitForInput();
+      },
+      error(reason) {
+        _reject(reason);
+        waitForInput();
+      },
+    };
+    subscribe(observer);
     waitForInput();
   }
   [Symbol.asyncIterator]() {
     // This function may be called more than once, allowing for multiple consumers
     // Client code may stop consuming (calling next()) at any time, so values are not stored up after the last promise returned from next() is settled.
+    // Client code that is still consuming is responsible for calling next() as soon as possible after the last return value has settled.
     return {
       next: () => {
         return new Promise((resolve, reject) => {
@@ -47,5 +51,11 @@ class EventIterable {
         });
       },
     };
+  }
+}
+
+export async listen(iterable, handler) {
+  for await (const event of iterable) {
+    handler(event);
   }
 }
